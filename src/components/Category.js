0264caac33a219537fe15eac86e993c1dd0cc9b0
kramstyles <mark.eke@decagon.dev>
2022-05-14 21:22:02 +0100

    spaced icons on nav

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,27 +1,45 @@
 import {FaPizzaSlice} from "react-icons/fa";
 import {GiChopsticks, GiRank3} from "react-icons/gi";
 import {IoFastFood} from "react-icons/io5";
-// import styled from "styled-components";
+import {NavLink} from "react-router-dom";
+import styled from "styled-components";
+
+
+const List = styled.div`
+    display: flex;
+    justify-content: space-evenly;
+    margin: 2rem 0rem;
+    background-color: silver;
+    
+    div{
+        text-align: center;
+    }
+    
+`
 
 export default function Category(){
     return (
-        <div>
+        <List>
             <div>
+            {/*<NavLink to={'/cuisine/Italian'}>*/}
                 <FaPizzaSlice/>
                 <h4>Italian</h4>
             </div>
             <div>
+            {/*<NavLink to={'/cuisine/Favorites'}>*/}
                 <GiRank3/>
                 <h4>Favorites</h4>
             </div>
             <div>
+            {/*<NavLink to={'/cuisine/Chinese'}>*/}
                 <GiChopsticks/>
                 <h4>Chinese</h4>
             </div>
             <div>
+            {/*<NavLink to={'/cuisine/Intercontinental'}>*/}
                 <IoFastFood/>
                 <h4>Intercontinental</h4>
             </div>
-        </div>
+        </List>
     )
 }
