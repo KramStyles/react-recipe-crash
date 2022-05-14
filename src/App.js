27c84aeabb4ccd02fbe5beaf80d33a545fb95682
kramstyles <mark.eke@decagon.dev>
2022-05-14 09:15:39 +0100

    added page and component folder

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import Home from "./pages/Home";
+
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Hello</h1>
+            <Home/>
+        </div>
+    );
 }
 
 export default App;
