51e474e14c2b98970a4d268452e6b517bd8ce748
kramstyles <mark.eke@decagon.dev>
2022-05-14 14:25:23 +0100

    getting recipe title

--- src/components/Popular.js
+++ src/components/Popular.js
@@ -1,6 +1,8 @@
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 
 export default function Popular() {
+    const [popular, setPopular] = useState([]);
+
     // Run function as soon as the component gets loaded
     useEffect(() => {
         getPopular();
@@ -9,17 +11,25 @@ export default function Popular() {
     const getPopular = async () => {
         let data = undefined;
         if (localStorage.getItem('random_recipes')) {
-            data = JSON.parse(localStorage.getItem('random_recipes'))
+            data = JSON.parse(localStorage.getItem('random_recipes'));
+            console.log('From localstorage', data)
         } else {
             const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=18`);
             data = await api.json();
             localStorage.setItem('random_recipes', JSON.stringify(data))
+            console.log('From api', data)
         }
-        console.log(data, " is data");
+
+        setPopular(data.recipes);
     }
     return (
         <div>
             Popular
+            {popular.map(recipe => {
+                return (
+                  <pre>{recipe.title}</pre>  
+                );
+            })}
         </div>
     )
 }
