371088885b8ec5b40d6d556b73d5541d33730a9e
kramstyles <mark.eke@decagon.dev>
2022-05-14 11:18:48 +0100

    added condition to checkmate random apis for testing

--- src/components/Popular.js
+++ src/components/Popular.js
@@ -1,5 +1,23 @@
-export default function Popular (){
-    return(
+import {useEffect} from "react";
+
+export default function Popular() {
+    // Run function as soon as the component gets loaded
+    useEffect(() => {
+        getPopular();
+    }, []);
+
+    const getPopular = async () => {
+        let data = undefined;
+        if (localStorage.getItem('random_recipes')) {
+            data = JSON.parse(localStorage.getItem('random_recipes'))
+        } else {
+            const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=18`);
+            data = await api.json();
+            localStorage.setItem('random_recipes', JSON.stringify(data))
+        }
+        console.log(data, " is data");
+    }
+    return (
         <div>
             Popular
         </div>
