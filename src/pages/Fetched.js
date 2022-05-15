88393dbba85de5a71d672e148088da43bb4c840d
kramstyles <mark.eke@decagon.dev>
2022-05-16 00:27:57 +0100

    passing search query to console

--- src/pages/Fetched.js
+++ src/pages/Fetched.js
@@ -1,8 +1,14 @@
 import {useParams} from "react-router-dom";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
-export default function Fetched () {
+export default function Fetched() {
     const [query, setQuery] = useState([])
+    let params = useParams();
+
+    useEffect(() => {
+        // getQuery(params.search);
+        console.log(params.search);
+    }, [params.search]); // Use effect would mount when there's a change in Params
 
     const getQuery = async (name) => {
         let data = localStorage.getItem(`${name}_query`);
@@ -20,7 +26,7 @@ export default function Fetched () {
 
     return (
         <div>
-            <h1>This is the fetched page. Let's get {useParams().search}</h1>
+            <h1>This is the fetched page. Let's get {params.search}</h1>
         </div>
     )
 }
