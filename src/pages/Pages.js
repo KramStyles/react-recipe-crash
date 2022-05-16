b051e7f7375c93380c04cdf0a4c7f8ec87d216f6
kramstyles <mark.eke@decagon.dev>
2022-05-16 21:51:57 +0100

    created Recipe page

--- src/pages/Pages.js
+++ src/pages/Pages.js
@@ -3,6 +3,7 @@ import Category from "../components/Category";
 import Cuisine from "./Cuisine";
 import Fetched from "./Fetched";
 import Home from "./Home";
+import Recipe from "./Recipe";
 import Search from "../components/Search";
 
 export default function Pages() {
@@ -14,6 +15,7 @@ export default function Pages() {
                 <Route path={'/'} element={<Home/>}/>
                 <Route path={'/cuisine/:type'} element={<Cuisine/>}/>
                 <Route path={'/searched/:search'} element={<Fetched/>}/>
+                <Route path={'/recipe/:name'} element={<Recipe />} />
             </Routes>
         </BrowserRouter>
     )
